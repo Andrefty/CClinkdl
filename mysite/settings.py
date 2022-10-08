37feb0dd87075eaea8db52be8b7366c6d95b0c71
Andrefty <39839328+Andrefty@users.noreply.github.com>
2022-10-08 23:48:19 +0300

    Switched debug mode off

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_^y7k=ujf7f-*tlfqxsx*9wz16(ckqvs9-&wf_ir_e!ai&kvh('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
